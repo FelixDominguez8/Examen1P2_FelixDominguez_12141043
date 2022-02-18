88f22c3e28e513d876cc725524fbf88f6d4d061f
Felix Dominguez <66543823+StickyFingers84@users.noreply.github.com>
2022-02-18 16:51:29 -0600

    Sexto Commit
    Commit para entrega temporal del link

--- src/examen1p2_felixdominguez_12141043/Examen1P2_FelixDominguez_12141043.java
+++ src/examen1p2_felixdominguez_12141043/Examen1P2_FelixDominguez_12141043.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package examen1p2_felixdominguez_12141043;
+import java.util.ArrayList;
 import java.util.Scanner;
 /**
  *
@@ -17,6 +18,10 @@ public class Examen1P2_FelixDominguez_12141043 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        ArrayList<Universo> universos=new ArrayList();
+        ArrayList<Escuadron> escuadrones=new ArrayList();
+        ArrayList<Persona> heroes=new ArrayList();
+        ArrayList<Persona> villanos=new ArrayList();
         char resp='s';
         while(resp=='s'){
             System.out.println("");
@@ -41,19 +46,44 @@ public class Examen1P2_FelixDominguez_12141043 {
                     System.out.println("");
                     switch(opcion2){
                         case 1:{
-                            System.out.println("");
+                            System.out.println("Ingrese le nombre del universo: ");
+                            lea.nextLine();
+                            String nombre=lea.nextLine();
+                            universos.add(new Universo(nombre));
                             break;
                         }
                         case 2:{
+                            System.out.println("Ingrese la posicion del universo a modificar: ");
+                            int pos=lea.nextInt();
+                            System.out.println("Ingrese el nuevo nombre del universo");
+                            lea.nextLine();
+                            String nombre=lea.nextLine();
+                            ((Universo)universos.get(pos)).setNombre(nombre);
                             break;
                         }
                         case 3:{
+                            System.out.println("Ingrese la posicion del universo a modificar: ");
+                            int pos=lea.nextInt();
+                            universos.remove(pos);
                             break;
                         }
                         case 4:{
+                            String salida="";
+                            for (Object temp : universos) {
+                                salida+=""+universos.indexOf(temp)+" - "+temp+"\n";
+                            }
+                            System.out.println(salida);
                             break;
                         }
                         case 5:{
+                            System.out.println("Ingrese la posicion del universo la cual se le asignara el escuadron: ");
+                            int pos=lea.nextInt();
+                            System.out.println("Ingrese la posicion del escuadron que sera asignado: ");
+                            int pos2=lea.nextInt();
+                            ((Universo)universos.get(pos)).getEscuadrones().add(escuadrones.get(pos2));
+                            break;
+                        }
+                        case 6:{
                             break;
                         }
                     }
@@ -83,6 +113,114 @@ public class Examen1P2_FelixDominguez_12141043 {
                     System.out.println("Ingrese la opcion que desea: ");
                     int opcion2=lea.nextInt();
                     System.out.println("");
+                    System.out.println("1) Persona normal");
+                    System.out.println("2) Mutante");
+                    System.out.println("3) Persona con accidente radioactivo");
+                    System.out.println("4) Superhumano");
+                    System.out.println("5) Deidad");
+                    System.out.println("6) Alien");
+                    System.out.println("Escoja el tipo de Persona: ");
+                    int tipo=lea.nextInt();
+                    System.out.println("");
+                    switch(opcion2){
+                        case 1:{
+                            System.out.println("Ingrese el nombre de la persona: ");
+                            lea.nextLine();
+                            String nombre=lea.nextLine();
+                            System.out.println("Ingrese el poder de la persona: ");
+                            String poder=lea.nextLine();
+                            System.out.println("Ingrese la debilidad de la persona: ");
+                            String debilidad=lea.nextLine();
+                            System.out.println("¿Es un heroe o es un villano? heroe/villano: ");
+                            String heroeovillano=lea.nextLine();
+                            System.out.println("Ingrese su fuerza: ");
+                            int fuerza=lea.nextInt();
+                            System.out.println("Ingrese su cantidad de habilidad fisica: ");
+                            int fisica=lea.nextInt();
+                            System.out.println("Ingrese su cantidad de habilidad mental");
+                            int mental=lea.nextInt();
+                            System.out.println("¿Esta persona tiene escuadron? true/false:");
+                            boolean tiene=lea.nextBoolean();
+                            switch(tipo){
+                                case 1:{
+                                    if(heroeovillano.equals("heroe")){
+                                        heroes.add(new PersonaNormal(nombre,poder,debilidad,heroeovillano,fuerza,fisica,mental,tiene));
+                                    }else if(heroeovillano.equals("villano")){
+                                        villanos.add(new PersonaNormal(nombre,poder,debilidad,heroeovillano,fuerza,fisica,mental,tiene));
+                                    }
+                                    break;
+                                }
+                                case 2:{
+                                    char s='s';
+                                    ArrayList<String> factores=new ArrayList();
+                                    while(s=='s'){
+                                        System.out.println("Ingrese uno de sus factores: ");
+                                        lea.nextLine();
+                                        String factor=lea.nextLine();
+                                        factores.add(factor);
+                                        System.out.println("Desea ingresar otro factor (s/n): ");
+                                        s=lea.next().charAt(0);
+                                    }
+                                    if(heroeovillano.equals("heroe")){
+                                        heroes.add(new Mutante(factores,nombre,poder,debilidad,heroeovillano,fuerza,fisica,mental,tiene));
+                                    }else if(heroeovillano.equals("villano")){
+                                        villanos.add(new Mutante(factores,nombre,poder,debilidad,heroeovillano,fuerza,fisica,mental,tiene));
+                                    }
+                                    break;
+                                }
+                                case 3:{
+                                    System.out.println("Ingrese la edad de la persona cuando tuvo el accidente: ");
+                                    int edad=lea.nextInt();
+                                    System.out.println("Ingrese el tipo de accidente que fue: ");
+                                    lea.nextLine();
+                                    String tipoaccidente=lea.nextLine();
+                                    if(heroeovillano.equals("heroe")){
+                                        heroes.add(new PorAccidenteRadioactivo(edad,tipoaccidente,nombre,poder,debilidad,heroeovillano,fuerza,fisica,mental,tiene));
+                                    }else if(heroeovillano.equals("villano")){
+                                        villanos.add(new PorAccidenteRadioactivo(edad,tipoaccidente,nombre,poder,debilidad,heroeovillano,fuerza,fisica,mental,tiene));
+                                    }
+                                    break;
+                                }
+                                case 4:{
+                                    if(heroeovillano.equals("heroe")){
+                                        heroes.add(new PersonaNormal(nombre,poder,debilidad,heroeovillano,fuerza,fisica,mental,tiene));
+                                    }else if(heroeovillano.equals("villano")){
+                                        villanos.add(new PersonaNormal(nombre,poder,debilidad,heroeovillano,fuerza,fisica,mental,tiene));
+                                    }
+                                    break;
+                                }
+                                case 5:{
+                                    if(heroeovillano.equals("heroe")){
+                                        heroes.add(new PersonaNormal(nombre,poder,debilidad,heroeovillano,fuerza,fisica,mental,tiene));
+                                    }else if(heroeovillano.equals("villano")){
+                                        villanos.add(new PersonaNormal(nombre,poder,debilidad,heroeovillano,fuerza,fisica,mental,tiene));
+                                    }
+                                    break;
+                                }
+                                case 6:{
+                                    if(heroeovillano.equals("heroe")){
+                                        heroes.add(new PersonaNormal(nombre,poder,debilidad,heroeovillano,fuerza,fisica,mental,tiene));
+                                    }else if(heroeovillano.equals("villano")){
+                                        villanos.add(new PersonaNormal(nombre,poder,debilidad,heroeovillano,fuerza,fisica,mental,tiene));
+                                    }
+                                    break;
+                                }
+                            }
+                            break;
+                        }
+                        case 2:{
+                            break;
+                        }
+                        case 3:{
+                            break;
+                        }
+                        case 4:{
+                            break;
+                        }
+                        case 5:{
+                            break;
+                        }
+                    }
                     break;
                 }
                 case 4:{
